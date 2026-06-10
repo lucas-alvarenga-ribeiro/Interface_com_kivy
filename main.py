8f9d5930d9556c8529999d8ea9e6eec1b169a37a
Lucas Ribeiro <lucasalvarengaribeiro.lar@gmail.com>
2026-06-10 15:12:18 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from clientemodbus import ClienteMODBUS
+from classes_widget import BasicApp
+from kivy.config import Config
+
+
+#c = ClienteMODBUS('localhost',502)
+
+if __name__ == '__main__':
+    Config.set('graphics','resizable',True)
+    BasicApp().run()
+
+
+#ci = ClienteInterface(c)
+#ci.atendimento()
+
